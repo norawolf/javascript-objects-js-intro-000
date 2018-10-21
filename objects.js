9eb0c90760b48aaf8520c34ed26437641ddf4013
Nora Vogt <noraevogt@gmail.com>
2018-10-21 19:20:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,11 @@
 var playlist = { artistName: 'songTitle'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
+  playlist[artistName] = songTitle;
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete [artistName] = songTitle;
   return playlist
 }
