1235a426f6e042c7a02e44b5982228da3d65a380
Nora Vogt <noraevogt@gmail.com>
2018-10-21 19:18:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,6 @@
 var playlist = { artistName: 'songTitle'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.Slowdive = 'Alison';
-  playlist.MyBloodyValentine = 'Sometimes';
-  return (playlist, 'Phil Ochs', "Here's to the State of Mississippi");
+  playlist[artistName] = songTitle
+  return playlist
 }
